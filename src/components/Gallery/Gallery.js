e6f1ee3f9349708b354a04a343ab454821b04b1f
yousseftalibi <yousseftalibi@outlook.com>
2023-04-24 13:24:13 +0200

    websocket instant chatting

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,15 +1,69 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import React from 'react';
 import './Gallery.css';
 
 const Gallery = () => {
+  const [messages, setMessages] = useState([]);
+  const [inputMessage, setInputMessage] = useState('');
+  const ws = useRef(null);
 
+  useEffect(() => {
+    ws.current = new WebSocket("ws://localhost:8080/chat");
+
+    ws.current.onopen = () => {
+      console.log('WebSocket connected');
+    };
+
+    ws.current.onmessage = (event) => {
+      const message = JSON.parse(event.data);
+      setMessages((prevMessages) => [...prevMessages, message]);
+    };
+
+    return () => {
+      if (ws.current) {
+        ws.current.close();
+      }
+    };
+  }, []);
+
+  const sendMessage = () => {
+    const newMessage = {
+      id: "3000",
+      message: inputMessage,
+    };
+      
+      ws.current.send(JSON.stringify(newMessage));
+      setInputMessage('');
+    
+  };
 
   return (
-    <div className='contents'>
-      <p> Gallery </p>
+    <div className="contents">
+      <p>Chat box, here you can send and receive messages to your friend!</p>
+      <br />
+      <div className="chatbox">
+        <div className="chatbox__message">
+          {messages.map((message, index) => (
+            <div key={index}>
+              {' '}
+            {message.message}
+            </div>
+          ))}
+        </div>
+      </div>
+      <br />
+      <div>
+        <input
+          type="text"
+          placeholder="Type your message here"
+          value={inputMessage}
+          onChange={(e) => setInputMessage(e.target.value)}
+        />
+        <br />
+        <button onClick={sendMessage}>Send</button>
+      </div>
     </div>
   );
-}
+};
 
 export default Gallery;
