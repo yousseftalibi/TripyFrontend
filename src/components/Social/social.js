394ccc225e7b8cb61f7cdda54dbc22c1c36a793a
yousseftalibi <yousseftalibi@outlook.com>
2023-05-25 22:43:24 +0200

    social, websocket, stop searching

--- src/components/Social/social.js
+++ src/components/Social/social.js
@@ -1,76 +1,74 @@
 import { AgGridReact } from 'ag-grid-react';
-import {React, useState} from 'react';
+import {React} from 'react';
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-alpine.css';
 import { Async } from 'react-async';
-import { useCallback, useRef } from 'react';
+import { useCallback, useRef, useEffect, useState } from 'react';
 
 const Social = () => {
 
-    const [rowData, setRowData] = useState();
+    const [rowData, setRowData] = useState([]);
+    const wsPlaces = useRef(null);
     let gridApi = useRef(null);
+    const [streaming, setStreaming] = useState(true);
+
+    useEffect(() => {
+      wsPlaces.current = new WebSocket("ws://localhost:8083/places");
+      wsPlaces.current.onopen = () => {
+        wsPlaces.current.send("paris");
+      };
+      wsPlaces.current.onmessage = (event) => {
+        const place = JSON.parse(event.data);
+        console.log("received message");
+        console.table(place);
+        setRowData((prevRowData) => [...prevRowData, place]);
+      };
+      return () => {
+        if (wsPlaces.current) {
+          wsPlaces.current.close();
+        }
+      };
+    }, []);
+
 
     const columnDefs = [
-        { headerName: 'Name', field: 'name', resizable: true, rowDrag: true },
-        { headerName: 'Rate', field: 'rate', resizable: true, sortable: true },
-        { headerName: 'Kinds', field: 'kinds', resizable: true },
-        { headerName: 'Dist', field: 'dist', resizable: true },
-        { headerName: 'Osm', field: 'osm', resizable: true },
-        { headerName: 'Wikidata', field: 'wikidata', resizable: true },
-        { headerName: 'Xid', field: 'xid', resizable: true },
-      ];
+      { headerName: 'Name', field: 'name', resizable: true },
+      { headerName: 'Rate', field: 'rate', resizable: true, sortable: true },
+      { headerName: 'Category', field: 'kinds', resizable: true },
+      { headerName: 'Distance from you', field: 'dist', resizable: true },
+    ];
 
     const onGridReady = useCallback((params) => {
        gridApi.current = params.api;
        gridApi.current?.sizeColumnsToFit();
       }, [])
 
-  
-    const getApi = useCallback ( async () => {
-        const options = {
-            method: 'GET'
-        }
-        const response = await fetch('http://localhost:8083/api/normalController/paris', options);
-        const data = await response.json();
-        console.table(data);
-       gridApi.current.setRowData(data)     
-    }, []);
-    
 
- 
-    const onRowDragEnd = (event) => {
-        const newRowData = [...rowData];
-        const movingItem = newRowData[event.node.rowIndex];
-      
-        newRowData.splice(event.node.rowIndex, 1);
-        newRowData.splice(event.overIndex, 0, movingItem);
-      
-        newRowData.forEach((row, index) => {
-          row.priority = index + 1;
-        });
-        
-        setRowData(newRowData);
-      };
-      
+    const toggleStreaming = () => {
+        wsPlaces.current.send("stop");
+        setStreaming(false);
+    };
+
     return(
-    <Async promiseFn={getApi}>
-            <p>You can order the rows in order to priortize an activity. The activity at the top represents what you would like to do the most.</p>
-            <br/>
-            <div style={ {display: 'flex', justifyContent:'center', alignItems:'center'}}>
-                <div className='ag-theme-alpine' style={ {width: '1000px', height:'400px'}}>
-                    <AgGridReact
-                        rowData={rowData}
-                        columnDefs={columnDefs}
-                        animateRows={true}
-                        onRowDragEnd={onRowDragEnd}
-                        onGridReady={onGridReady}
-                    > </AgGridReact>
-                </div>
-            </div>
-            <button type="submit"> Get Iternary </button>
-    </Async>
-    )
+      <Async>
+        <p>We are search for interesting places to visit. You can stop the search when you're satisfied with the result. </p>
+              <div style={ {display: 'flex', justifyContent:'center', alignItems:'center'}}>
+                  <div className='ag-theme-alpine' style={ {width: '1000px', height:'400px'}}>
+                      <AgGridReact
+                          rowData={rowData}
+                          columnDefs={columnDefs}
+                          onGridReady={onGridReady}
+                      > </AgGridReact>
+                  </div>
+              </div>
+              { streaming &&
+                <button type="button" onClick={toggleStreaming}> Stop Searching </button>
+               } 
+              { !streaming &&
+                <button type="submit"> Get Iternary </button>
+              }
+          </Async>
+      )
 }
-
 
 export default Social;
