c2765b5480c69dbd30c0a682a4749c07602ca17d
yousseftalibi <yousseftalibi@outlook.com>
2023-05-26 04:16:59 +0200

    table with map, iternary remaining

---
+++ src/components/Map/GoogleMapsApi.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
+const containerStyle = {
+    width: '40vw',
+    height: '48vh',
+   
+
+};
+
+const MapComponent = (props) => {
+      
+    const { isLoaded } = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: "apikey"
+    })
+ 
+    const places = props.places;
+    
+
+    return isLoaded ? (
+        <>
+      <GoogleMap
+            mapContainerStyle={containerStyle}
+            zoom={12}
+            center={{ lat: places[places.length - 1].lat, lng: places[places.length - 1].lon }}
+            >
+        {places && places.map((place, index) => (
+          <Marker
+            key={index}
+            position={{ lat: place.lat, lng: place.lon }}
+          />
+            ))}
+      </GoogleMap>
+          
+  </>
+) : (
+  <></>
+);
+}
+
+export default MapComponent;  
--- src/components/Social/social.js
+++ src/components/Social/social.js
@@ -1,74 +1,117 @@
 import { AgGridReact } from 'ag-grid-react';
-import {React} from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-alpine.css';
 import { Async } from 'react-async';
-import { useCallback, useRef, useEffect, useState } from 'react';
-
+import MapComponent from '../Map/GoogleMapsApi';
 const Social = () => {
 
-    const [rowData, setRowData] = useState([]);
+    const [filterOptions, setFilterOptions] = useState([]);
     const wsPlaces = useRef(null);
     let gridApi = useRef(null);
     const [streaming, setStreaming] = useState(true);
+    const [places, setPlaces] = useState([]);
+
+    const fetchPlaceDetails = async (placeId) => {
+        const url = `https://opentripmap-places-v1.p.rapidapi.com/en/places/xid/${placeId}`;
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': 'c4d4c4a3afmsh8073c2210da8497p1bf278jsne8174b51a3ec',
+                'X-RapidAPI-Host': 'opentripmap-places-v1.p.rapidapi.com'
+            }
+        };
+
+        const response = await fetch(url, options);
+        const result = await response.json();
+
+        return result.point;
+    };
 
     useEffect(() => {
       wsPlaces.current = new WebSocket("ws://localhost:8083/places");
       wsPlaces.current.onopen = () => {
         wsPlaces.current.send("paris");
       };
-      wsPlaces.current.onmessage = (event) => {
-        const place = JSON.parse(event.data);
+      wsPlaces.current.onmessage = async (event) => {
+        let place = JSON.parse(event.data);
+     
+        const point = await fetchPlaceDetails(place.xid);
+        place = { ...place, ...point }; 
         console.log("received message");
         console.table(place);
-        setRowData((prevRowData) => [...prevRowData, place]);
+
+        const kinds = place.kinds.split(',');
+        kinds.forEach((kind) => {
+          if (!filterOptions.includes(kind)) {
+            setFilterOptions(prevOptions => [...prevOptions, kind]);
+          }
+        });
+        setPlaces(prevPlaces => [...prevPlaces, place]);
+
       };
       return () => {
         if (wsPlaces.current) {
           wsPlaces.current.close();
         }
       };
-    }, []);
-
+    }, [filterOptions]);
 
     const columnDefs = [
-      { headerName: 'Name', field: 'name', resizable: true },
-      { headerName: 'Rate', field: 'rate', resizable: true, sortable: true },
-      { headerName: 'Category', field: 'kinds', resizable: true },
-      { headerName: 'Distance from you', field: 'dist', resizable: true },
+      { headerName: 'Name', field: 'name', resizable: true, checkboxSelection: true },
+      { headerName: 'Rate (0-7)', field: 'rate', resizable: true, sortable: true },
+      { headerName: 'Category (Filter)', field: 'kinds', resizable: true, filter: 'agSetColumnFilter', filterParams: { values: filterOptions } },
+      { headerName: 'Distance from you (meters)', field: 'dist', resizable: true },
     ];
 
     const onGridReady = useCallback((params) => {
        gridApi.current = params.api;
        gridApi.current?.sizeColumnsToFit();
-      }, [])
-
+    }, [])
 
     const toggleStreaming = () => {
         wsPlaces.current.send("stop");
         setStreaming(false);
     };
+   
 
     return(
       <Async>
-        <p>We are search for interesting places to visit. You can stop the search when you're satisfied with the result. </p>
-              <div style={ {display: 'flex', justifyContent:'center', alignItems:'center'}}>
-                  <div className='ag-theme-alpine' style={ {width: '1000px', height:'400px'}}>
-                      <AgGridReact
-                          rowData={rowData}
-                          columnDefs={columnDefs}
-                          onGridReady={onGridReady}
-                      > </AgGridReact>
-                  </div>
+        <style>{`
+            .ag-header-cell-menu-button {
+                opacity: 1 !important;
+            }
+        `}</style>
+    
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      
+          <div style={{ display: 'flex', justifyContent:'center', alignItems:'center', width: '100%' }}>
+              <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                { places.length > 0 && <MapComponent places={places} />}
+                  <button type='button' style={{visibility: !streaming && places.length > 0 ? 'visible' : 'hidden'}}>Get Itinerary</button>
+
               </div>
-              { streaming &&
-                <button type="button" onClick={toggleStreaming}> Stop Searching </button>
-               } 
-              { !streaming &&
-                <button type="submit"> Get Iternary </button>
-              }
-          </Async>
-      )
+              
+              <div className='ag-theme-alpine pl-5' style={{ width: '1020px', height:'400px' }}>
+                
+                  <AgGridReact
+                    rowData={places}
+                    columnDefs={columnDefs}
+                    onGridReady={onGridReady}
+                  />
+                  <h6 style={{ transform: 'translateY(-450px)' }}>Filter Category column to specify the type, historic period or activity</h6>
+
+                  {streaming && (
+                      <button type='button' onClick={toggleStreaming}>
+                          Stop Searching
+                      </button>
+                  )}
+              </div>
+          </div>
+        </div>
+      </Async>
+    )
+    
 }
 
 export default Social;
